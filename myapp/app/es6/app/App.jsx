241fc3f535586960e96e13bf581f8c52f9a1c244
Kararin <charms_devil@mail.ru>
2016-03-28 23:01:16 +0300

    react-bootrap added

--- myapp/app/es6/app/App.jsx
+++ myapp/app/es6/app/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {gotoPage} from './actions/currentPage';
-import Tabs from './core/components/tabs/Tabs.jsx';
-import Tab from './core/components/tabs/Tab.jsx';
+import {Tabs, Tab} from 'react-bootstrap';
 import Main from './app/Main.jsx';
 
 export default () => {
     return (
-        <div>
-            Kararin
-        </div>
-    );
+  <Tabs defaultActiveKey={1}>
+    <Tab  eventKey={1} title="Tab 1">Tab 1 content</Tab>
+    <Tab eventKey={2} title="Tab 2">Tab 2 content</Tab>
+  </Tabs>
+);
 }
