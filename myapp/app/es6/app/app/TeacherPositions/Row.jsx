2b39c7782a470879f27dd2d7903cb2ef66635290
Kararin <charms_devil@mail.ru>
2016-04-07 21:46:38 +0300

    Row view changed to class

--- myapp/app/es6/app/app/TeacherPositions/Row.jsx
+++ myapp/app/es6/app/app/TeacherPositions/Row.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
 import {Button, Glyphicon} from 'react-bootstrap';
 
-const Row = ({
-    position,
-    actions,
-    onDelete,
-    isEdit,
-    beginEdit,
-    onEdit
-}) => (
-    <tr>
-        <td>{position.name}</td>
-        <td>{position.shortName}</td>
-        <td>
-            <Button>
-                <Glyphicon glyph="edit"/>
-            </Button>
-        </td>
-        <td>
-            <Button onClick = {() => {onDelete(position);}}>
-                <Glyphicon glyph="remove"/>
-            </Button>
-        </td>
-    </tr>
-);
+export default class Row extends React.Component {
+    render() {
+        var {position, onDelete} = this.props;
+        return (
+            <tr>
+                <td>
+                    {position.name}
+                </td>
+                <td>
+                    {position.shortName}
+                </td>
+                <td>
+                    <Button onClick = {() => onDelete(position)}>
+                        <Glyphicon glyph='remove'/>
+                    </Button>
+                </td>
+            </tr>
+        );
+    }
+};
 
-//TODO: default props
-export default Row;
+Row.defaultProps = {
+    position: {},
+    onDelete: console.log('onDelete not implemented'),
+    isEdit: false,
+    beginEdit: console.log('beginEdit not implemented'),
+    onEdit: console.log('onEdit not implemented')
+};
+
+Row.propTypes = {
+    position: React.PropTypes.object,
+    onDelete: React.PropTypes.func,
+    isEdit: React.PropTypes.bool,
+    beginEdit: React.PropTypes.func,
+    onEdit: React.PropTypes.func
+};
